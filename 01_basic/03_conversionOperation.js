dc19277196c572385d36d7e64ec0b5e6613dbcc5
bhargavhingu1122-dot <bhargavhingu1122@gmail.com>
2026-06-19 07:09:45 +0000

    Conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+ let score = false
+// console.log(typeof score);
+
+
+ let valueInNUmber = Number(score)
+// console.log( valueInNUmber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Bhargav" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+let myName = 33
+
+let stringName = String(myName)
+console.log(stringName);
+console.log(typeof stringName);
