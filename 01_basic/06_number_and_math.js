3c43491324df533a948aed031fe757f91ae404f7
bhargavhingu1122-dot <bhargavhingu1122@gmail.com>
2026-06-24 06:53:48 +0000

    Math and Num

--- 01_basic/06_number_and_math.js
+++ 01_basic/06_number_and_math.js
@@ -1,20 +1,51 @@
 const score = 400
-console.log(score);
-console.log(typeof score);
+// console.log(score);
+// console.log(typeof score);
 
 
 
 const balance = new Number(1000)
-console.log(balance);
+// console.log(balance); 
 
 
-console.log(balance.toString());
-console.log(typeof balance);
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(typeof balance);                //object
+// console.log(balance.toFixed(2));
 
 const othervalue = 100.333
-console.log(othervalue.toPrecision(4));
+// console.log(othervalue.toPrecision(4));    // 100.3
 
 const indianHunderds = 100000000
-console.log(indianHunderds.toLocaleString('en-in'));
+// console.log(indianHunderds.toLocaleString('en-in'));     //10,00,00,000
+
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math);   //Object
+// console.log(Math.abs(-4));      //Positive value
+// console.log(Math.round(4.7));   //Round figure ..... Most usage
+// console.log(Math.ceil(4.7));    // Upper Round figure value
+// console.log(Math.floor(4.7));   // Lower round figure value
+// console.log(Math.min(4, 5, 1, 7));  // Minimum value
+// console.log(Math.max(4, 5, 1, 7));  // Maximum value
+
+
+console.log(Math.random());                          // 0 or 1
+console.log((Math.random()*10) + 1);                 //  0 or 1 ... Random into(*) 10 + 1
+console.log(Math.floor(Math.random()*10) + 1);       // Value that lower round figure ''
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
 
